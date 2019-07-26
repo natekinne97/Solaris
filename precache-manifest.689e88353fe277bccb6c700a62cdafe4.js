86a52e6b0f290c447148712d9e855952c86105f7
Nate Kinne <nathan.kinne@gmail.com>
2019-07-25 22:19:50 -0500

    Updates

---
+++ precache-manifest.689e88353fe277bccb6c700a62cdafe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babad4e805e8526f7ecec1366156235a",
+    "url": "/Solaris/index.html"
+  },
+  {
+    "revision": "cb68b7020600dae6714b",
+    "url": "/Solaris/static/css/main.9d31f50a.chunk.css"
+  },
+  {
+    "revision": "6a319a3ad634e406283b",
+    "url": "/Solaris/static/js/2.90d84b74.chunk.js"
+  },
+  {
+    "revision": "cb68b7020600dae6714b",
+    "url": "/Solaris/static/js/main.f93e07cc.chunk.js"
+  },
+  {
+    "revision": "dad8d96cb9568bb31c55",
+    "url": "/Solaris/static/js/runtime~main.df9a9333.js"
+  }
+]);
